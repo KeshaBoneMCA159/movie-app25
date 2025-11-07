5f7a5eb414654ff95c8b7aa42975858da05bc54f
Kesha Bone <meadoremums@gmail.com>
2025-11-07 09:42:31 -0600

    created root route and setup error handling

---
+++ routes/router.js
@@ -0,0 +1,20 @@
+// Step 5 export router
+const express = require('express')
+const router = express.Router()
+const PORT = process.env.PORT || 3000
+
+// Step 6 create root route http://localhost:3000/api
+router.get('/api', (req, res)=> {
+    //res.send('movie api') <=Test 2
+    res.json({
+        'Movies': `http://localhost:${PORT}/api/movie`
+    })
+})
+// Step 7 error handling
+router.use((req, res, next)=> {
+    res.status(404)
+    // Test 3 type something random to make sure error message works(WORKS!!!)
+    .send('<h1> 404 Error: The princess is in another castle 🏰</h1>')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Step 1 build out my server
 const express = require('express')
 const server = express()
-const router = require('./routes/router')
+const router = require('./routes/router') // Step 4 (after commit) connect to router
 const PORT = process.env.PORT || 3000
 
 //Step 1b Install and handle security
@@ -9,8 +9,8 @@ const helmet = require('helmet')
 const cors = require('cors')
 
 // Step 2 Configure helmet
-//server.use(helmet())
-server.use(helmetcontentSecurityPolicy({
+// server.use(helmet())
+server.use(helmet.contentSecurityPolicy({
     useDefaults: true,
     crossOriginResourcePolicy: false,
      crossEmbedderPolicy: false,
@@ -23,6 +23,7 @@ server.use(helmetcontentSecurityPolicy({
 server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true}))
+server.use('/', router) // Step 4b import router
 
-//Step 1a 1st Test => Check terminal
+// Step 1a 1st Test => Check terminal
 server.listen(PORT, ()=> console.log(`It was working a second ago`))
